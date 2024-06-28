0bce282a3b4faaefb610201a276da74470a110dc
Pike86 <56256001+Pikerson@users.noreply.github.com>
2024-06-28 18:37:29 -0500

    Fix issues with backend module and Flask-Migrate

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,26 +1,27 @@
+# backend/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_mail import Mail
-from flask_cors import CORS
-from config import Config
+from .config import Config  # Use relative import
 
 db = SQLAlchemy()
 migrate = Migrate()
-mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
-    mail.init_app(app)
 
-    from .routes import auth_routes, contact_routes, portfolio_routes, thesis_routes
-    app.register_blueprint(auth_routes.auth_bp)
-    app.register_blueprint(contact_routes.contact_bp)
-    app.register_blueprint(portfolio_routes.portfolio_bp)
-    app.register_blueprint(thesis_routes.thesis_bp)
+    with app.app_context():
+        # Import parts of our application
+        from .routes import auth_routes, contact_routes, portfolio_routes, thesis_routes
+
+        # Register Blueprints
+        app.register_blueprint(auth_routes.bp)
+        app.register_blueprint(contact_routes.bp)
+        app.register_blueprint(portfolio_routes.bp)
+        app.register_blueprint(thesis_routes.bp)
 
     return app
