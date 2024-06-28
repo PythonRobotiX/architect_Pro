e32966b277ebb7e8d0dd7b66dfe5c59ee04eba19
Pike86 <56256001+Pikerson@users.noreply.github.com>
2024-06-28 17:20:44 -0500

    Initial commit

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
 from flask_mail import Mail
+from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,15 +11,22 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('backend.config.Config')
-    CORS(app)
+    app.config.from_object(Config)
     
     db.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+    CORS(app)
 
-    with app.app_context():
-        from backend import routes, models
-        db.create_all()
+    # Register Blueprints
+    from backend.routes.auth_routes import auth_bp
+    from backend.routes.contact_routes import contact_bp
+    from backend.routes.portfolio_routes import portfolio_bp
+    from backend.routes.thesis_routes import thesis_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(contact_bp)
+    app.register_blueprint(portfolio_bp)
+    app.register_blueprint(thesis_bp)
 
     return app
