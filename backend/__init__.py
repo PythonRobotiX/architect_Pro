130add0040142bc9d185c4ce90f8dbac22825942
Pike86 <56256001+Pikerson@users.noreply.github.com>
2024-06-29 13:34:24 -0500

    updated backend

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,27 +1,29 @@
-# backend/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .config import Config  # Use relative import
+from flask_mail import Mail
+from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
 
-    with app.app_context():
-        # Import parts of our application
-        from .routes import auth_routes, contact_routes, portfolio_routes, thesis_routes
+    from .routes.auth_routes import auth_bp
+    from .routes.contact_routes import contact_bp
+    from .routes.portfolio_routes import portfolio_bp
+    from .routes.thesis_routes import thesis_bp
 
-        # Register Blueprints
-        app.register_blueprint(auth_routes.bp)
-        app.register_blueprint(contact_routes.bp)
-        app.register_blueprint(portfolio_routes.bp)
-        app.register_blueprint(thesis_routes.bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(contact_bp)
+    app.register_blueprint(portfolio_bp)
+    app.register_blueprint(thesis_bp)
 
     return app
